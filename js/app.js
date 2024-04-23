81cf42b597fe097b73d0e9f1775829550723597b
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-23 15:31:02 +0200

    feat: bonus 5 done

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ Vue.createApp({
       searchedWord: "",
       newMessage: "",
       isWriting: false,
+      isOptionsClicked: false,
+      isContactEmpty: false,
       randomAnswers: [
         "dipende",
         "dubbi? domande?",
@@ -17,139 +19,139 @@ Vue.createApp({
         "Tutto bene!",
       ],
       contacts: [
-        {
-          name: "Michele",
-          avatar: "_1",
-          visible: true,
-          contact_status: "Ultimo accesso oggi alle ",
-          messages: [
-            {
-              date: "15:30",
-              message: "Hai portato a spasso il cane?",
-              status: "sent",
-            },
-            {
-              date: "15:50",
-              message: "Ricordati di stendere i panni",
-              status: "sent",
-            },
-            {
-              date: "16:15",
-              message: "Tutto fatto!",
-              status: "received",
-            },
-          ],
-        },
-        {
-          name: "Fabio",
-          avatar: "_2",
-          visible: true,
-          contact_status: "Ultimo accesso oggi alle ",
+        // {
+        //   name: "Michele",
+        //   avatar: "_1",
+        //   visible: true,
+        //   contact_status: "Ultimo accesso oggi alle ",
+        //   messages: [
+        //     {
+        //       date: "15:30",
+        //       message: "Hai portato a spasso il cane?",
+        //       status: "sent",
+        //     },
+        //     {
+        //       date: "15:50",
+        //       message: "Ricordati di stendere i panni",
+        //       status: "sent",
+        //     },
+        //     {
+        //       date: "16:15",
+        //       message: "Tutto fatto!",
+        //       status: "received",
+        //     },
+        //   ],
+        // },
+        // {
+        //   name: "Fabio",
+        //   avatar: "_2",
+        //   visible: true,
+        //   contact_status: "Ultimo accesso oggi alle ",
 
-          messages: [
-            {
-              date: "16:30",
-              message: "Ciao come stai?",
-              status: "sent",
-            },
-            {
-              date: "16:30",
-              message: "Bene grazie! Stasera ci vediamo?",
-              status: "received",
-            },
-            {
-              date: "16:35",
-              message: "Mi piacerebbe ma devo andare a fare la spesa.",
-              status: "sent",
-            },
-          ],
-        },
-        {
-          name: "Samuele",
-          avatar: "_3",
-          visible: true,
-          contact_status: "Ultimo accesso oggi alle ",
+        //   messages: [
+        //     {
+        //       date: "16:30",
+        //       message: "Ciao come stai?",
+        //       status: "sent",
+        //     },
+        //     {
+        //       date: "16:30",
+        //       message: "Bene grazie! Stasera ci vediamo?",
+        //       status: "received",
+        //     },
+        //     {
+        //       date: "16:35",
+        //       message: "Mi piacerebbe ma devo andare a fare la spesa.",
+        //       status: "sent",
+        //     },
+        //   ],
+        // },
+        // {
+        //   name: "Samuele",
+        //   avatar: "_3",
+        //   visible: true,
+        //   contact_status: "Ultimo accesso oggi alle ",
 
-          messages: [
-            {
-              date: "10:10",
-              message: "La Marianna va in campagna",
-              status: "received",
-            },
-            {
-              date: "10:20",
-              message: "Sicuro di non aver sbagliato chat?",
-              status: "sent",
-            },
-            {
-              date: "16:15",
-              message: "Ah scusa!",
-              status: "received",
-            },
-          ],
-        },
-        {
-          name: "Alessandro B.",
-          avatar: "_4",
-          visible: true,
-          contact_status: "Ultimo accesso oggi alle ",
+        //   messages: [
+        //     {
+        //       date: "10:10",
+        //       message: "La Marianna va in campagna",
+        //       status: "received",
+        //     },
+        //     {
+        //       date: "10:20",
+        //       message: "Sicuro di non aver sbagliato chat?",
+        //       status: "sent",
+        //     },
+        //     {
+        //       date: "16:15",
+        //       message: "Ah scusa!",
+        //       status: "received",
+        //     },
+        //   ],
+        // },
+        // {
+        //   name: "Alessandro B.",
+        //   avatar: "_4",
+        //   visible: true,
+        //   contact_status: "Ultimo accesso oggi alle ",
 
-          messages: [
-            {
-              date: "15:30",
-              message: "Lo sai che ha aperto una nuova pizzeria?",
-              status: "sent",
-            },
-            {
-              date: "15:50",
-              message: "Si, ma preferirei andare al cinema",
-              status: "received",
-            },
-          ],
-        },
-        {
-          name: "Alessandro L.",
-          avatar: "_5",
-          visible: true,
-          contact_status: "Ultimo accesso oggi alle ",
+        //   messages: [
+        //     {
+        //       date: "15:30",
+        //       message: "Lo sai che ha aperto una nuova pizzeria?",
+        //       status: "sent",
+        //     },
+        //     {
+        //       date: "15:50",
+        //       message: "Si, ma preferirei andare al cinema",
+        //       status: "received",
+        //     },
+        //   ],
+        // },
+        // {
+        //   name: "Alessandro L.",
+        //   avatar: "_5",
+        //   visible: true,
+        //   contact_status: "Ultimo accesso oggi alle ",
 
-          messages: [
-            {
-              date: "15:30",
-              message: "Ricordati di chiamare la nonna",
-              status: "sent",
-            },
-            {
-              date: "15:50",
-              message: "Va bene, stasera la sento",
-              status: "received",
-            },
-          ],
-        },
-        {
-          name: "Claudia",
-          avatar: "_6",
-          visible: true,
-          contact_status: "Ultimo accesso oggi alle ",
+        //   messages: [
+        //     {
+        //       date: "15:30",
+        //       message: "Ricordati di chiamare la nonna",
+        //       status: "sent",
+        //     },
+        //     {
+        //       date: "15:50",
+        //       message: "Va bene, stasera la sento",
+        //       status: "received",
+        //     },
+        //   ],
+        // },
+        // {
+        //   name: "Claudia",
+        //   avatar: "_6",
+        //   visible: true,
+        //   contact_status: "Ultimo accesso oggi alle ",
 
-          messages: [
-            {
-              date: "15:30",
-              message: "Ciao Claudia, hai novità?",
-              status: "sent",
-            },
-            {
-              date: "15:50",
-              message: "Non ancora",
-              status: "received",
-            },
-            {
-              date: "15:51",
-              message: "Nessuna nuova, buona nuova",
-              status: "sent",
-            },
-          ],
-        },
+        //   messages: [
+        //     {
+        //       date: "15:30",
+        //       message: "Ciao Claudia, hai novità?",
+        //       status: "sent",
+        //     },
+        //     {
+        //       date: "15:50",
+        //       message: "Non ancora",
+        //       status: "received",
+        //     },
+        //     {
+        //       date: "15:51",
+        //       message: "Nessuna nuova, buona nuova",
+        //       status: "sent",
+        //     },
+        //   ],
+        // },
         {
           name: "Federico",
           avatar: "_7",
@@ -197,49 +199,51 @@ Vue.createApp({
     };
   },
   created() {
-
-    this.contacts.forEach((curContact)=>{
-      console.log(curContact);
-      curContact.contact_status += curContact.messages[curContact.messages.length -1 ].date;
-    })
-  },  
+    this.contacts.forEach((curContact) => {
+      // console.log(curContact);
+      curContact.contact_status +=
+        curContact.messages[curContact.messages.length - 1].date;
+    });
+  },
   methods: {
     sendMessage() {
-      console.log("inviato");
-      let curHour = luxon.DateTime.now().setZone("Europe/Rome");
-      let formattedHour = curHour.toFormat("HH:mm");
-
-      if (this.newMessage !== "") {
-        this.contacts[this.activeChatIndex].messages.push({
-          date: formattedHour,
-          message: this.newMessage.trim(),
-          status: "sent",
-        });
-
-        setTimeout(() => {
-          const randNum =
-            Math.floor(Math.random() * this.randomAnswers.length - 1) + 1;
-          curHour = luxon.DateTime.now().setZone("Europe/Rome");
-          formattedHour = curHour.toFormat("HH:mm");
-          this.contacts[this.activeChatIndex].contact_status =
-            "Sta scrivendo...";
-          setTimeout(() => {
-            this.contacts[this.activeChatIndex].contact_status = "Online";
-            setTimeout(() => {
-              this.contacts[
-                this.activeChatIndex
-              ].contact_status = `ultimo accesso oggi alle ${formattedHour}`;
-            }, 4000);
-          }, 2000);
+      if (this.isContactEmpty !== true) {
+        let curHour = luxon.DateTime.now().setZone("Europe/Rome");
+        let formattedHour = curHour.toFormat("HH:mm");
+        if (this.newMessage !== "") {
+          this.contacts[this.activeChatIndex].messages.splice(0,1);
           this.contacts[this.activeChatIndex].messages.push({
             date: formattedHour,
-            message: this.randomAnswers[randNum],
-            status: "received",
+            message: this.newMessage.trim(),
+            status: "sent",
           });
-        }, 1000);
-      }
-
+  
+          setTimeout(() => {
+            const randNum =
+              Math.floor(Math.random() * this.randomAnswers.length - 1) + 1;
+            curHour = luxon.DateTime.now().setZone("Europe/Rome");
+            formattedHour = curHour.toFormat("HH:mm");
+            this.contacts[this.activeChatIndex].contact_status =
+              "Sta scrivendo...";
+            setTimeout(() => {
+              this.contacts[this.activeChatIndex].contact_status = "Online";
+              setTimeout(() => {
+                this.contacts[
+                  this.activeChatIndex
+                ].contact_status = `Ultimo accesso oggi alle ${formattedHour}`;
+              }, 4000);
+            }, 2000);
+            this.contacts[this.activeChatIndex].messages.push({
+              date: formattedHour,
+              message: this.randomAnswers[randNum],
+              status: "received",
+            });
+          }, 1000);
+        }
+      } 
       this.newMessage = "";
+      
+
     },
     handleSearch() {
       if (this.searchedWord !== "") {
@@ -259,7 +263,30 @@ Vue.createApp({
     },
     removeMessage(index) {
       this.contacts[this.activeChatIndex].messages.splice(index, 1);
-      console.log(this.contacts[0].messages);
     },
+    clearChat() {
+      this.contacts[this.activeChatIndex].messages = [{}]
+    },
+    deleteChat(){
+      console.log(this.contacts[this.activeChatIndex]); 
+      if (this.contacts.length - 1 !== 0) {
+        this.contacts.splice(this.activeChatIndex,1)
+        
+      } else {
+        this.contacts = [
+          {
+            name: "",
+            avatar: '',
+            visible: false,
+            contact_status: "",
+  
+            messages: [{}],
+          },
+        ]
+        this.isContactEmpty = true;
+        console.log("ciAO");  
+      }
+    },
+    createChat(){}
   },
 }).mount("#app");
